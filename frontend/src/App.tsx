6b4989d68e990baa3d2b004404585f60a27c771e
Fagner Cruz <fagner@sti.ufpb.br>
2023-09-11 12:57:55 -0300

    Rota Not Found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Catalog from "./Routes/ClientHome/Catalog";
 import ProductsDetails from "./Routes/ClientHome/ProductDetails";
 import ClientHome from "./Routes/ClientHome";
@@ -15,6 +15,7 @@ function App() {
             element={<ProductsDetails />}
           />
         </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
