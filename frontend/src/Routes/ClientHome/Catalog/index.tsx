660c2449a35f4e36a6fcfe3470a3f60406657cd8
Fagner Cruz <fagner@sti.ufpb.br>
2023-09-14 08:52:11 -0300

    Catalogo e Produto: Integração com Backend

--- frontend/src/Routes/ClientHome/Catalog/index.tsx
+++ frontend/src/Routes/ClientHome/Catalog/index.tsx
@@ -1,17 +1,27 @@
 import "./style.css";
-
 import SearchBar from "../../../components/SearchBar";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import CatalogItem from "../../../components/CatalogItem";
-import * as productService from "../../../services/product-service";
+import { ProductDTO } from "../../../types/Product";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Catalog() {
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products?size=12").then((response) => {
+      setProducts(response.data.content);
+      console.log("Status:" + response.status);
+    });
+  }, []);
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          {productService.findAll().map((p) => (
+          {products.map((p) => (
             <CatalogItem product={p} key={p.id} />
           ))}
         </div>
--- frontend/src/Routes/ClientHome/ProductDetails/index.tsx
+++ frontend/src/Routes/ClientHome/ProductDetails/index.tsx
@@ -1,11 +1,25 @@
 import "./styles.css";
 import ProductDetailCard from "../../../components/ProductDetailCard";
-import * as productService from "../../../services/product-service";
+//import * as productService from "../../../services/product-service";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../types/Product";
+import axios from "axios";
 
 export default function ProductsDetails() {
   const params = useParams();
-  const product = productService.findById(Number(params.productId));
+  //const product = productService.findById(Number(params.productId));
+
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/products/${params.productId}`)
+      .then((response) => {
+        console.log("Carregou produto: " + response.data.name);
+        setProduct(response.data);
+      });
+  }, [params.productId]);
 
   return <>{product && <ProductDetailCard product={product} />}</>;
 }
