86be5576ff954d90ff62fda5ee1267c6d9950d48
Fagner Cruz <fagner@sti.ufpb.br>
2023-09-14 12:54:34 -0300

    Tratamento de erro com catch

--- frontend/src/Routes/ClientHome/ProductDetails/index.tsx
+++ frontend/src/Routes/ClientHome/ProductDetails/index.tsx
@@ -1,20 +1,27 @@
 import "./styles.css";
 import ProductDetailCard from "../../../components/ProductDetailCard";
 import * as productService from "../../../services/product-service";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../types/Product";
 
 export default function ProductsDetails() {
   const params = useParams();
   const [product, setProduct] = useState<ProductDTO>();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    productService.findById(Number(params.productId)).then((response) => {
-      console.log("Carregou produto: " + response.data.name);
-      setProduct(response.data);
-    });
-  }, [params.productId]);
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        /* Condição para sucesso */
+        console.log("Carregou produto: " + response.data.name);
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
+  });
 
   return <>{product && <ProductDetailCard product={product} />}</>;
 }
